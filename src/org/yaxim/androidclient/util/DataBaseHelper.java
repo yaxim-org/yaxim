306fae23f5895a24e4f2d78ea301a347c232cbc4
Sven Pfleiderer <sg+github@roothausen.de>
2009-12-23 01:47:13 +0100

    Added public/private modifiers to DataBaseHelper

--- src/org/yaxim/androidclient/util/DataBaseHelper.java
+++ src/org/yaxim/androidclient/util/DataBaseHelper.java
@@ -7,12 +7,14 @@ import android.util.Log;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
-	static final String TAG = "DataBaseHelper";
-	static final String DATABASE_NAME = "yaxim.db";
-	static final int DATABASE_VERSION = 1;
-	static final String ACCOUNTS = "accounts";
-	static final String ROSTER = "roster";
-	static final String CHATS = "chats";
+	public static final String ACCOUNTS = "accounts";
+	public static final String ROSTER = "roster";
+	public static final String CHATS = "chats";
+
+	private static final String TAG = "DataBaseHelper";
+	private static final String DATABASE_NAME = "yaxim.db";
+	private static final int DATABASE_VERSION = 1;
+
 	
 	public DataBaseHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
