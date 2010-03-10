1cc7134d71e56d1defee17625c12bb1d14ca6ee7
Georg Lukas <georg@op-co.de>
2010-03-10 01:05:22 +0100

    move last connection error into own text view, displayed optionally

--- src/org/yaxim/androidclient/MainWindow.java
+++ src/org/yaxim/androidclient/MainWindow.java
@@ -74,7 +74,7 @@ public class MainWindow extends GenericExpandableListActivity {
 		createUICallback();
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.main);
-		mConnectingText = (TextView)findViewById(android.R.id.empty);
+		mConnectingText = (TextView)findViewById(R.id.error_view);
 		registerForContextMenu(getExpandableListView());
 	}
 
@@ -343,32 +343,33 @@ public class MainWindow extends GenericExpandableListActivity {
 
 	private void setConnectingStatus(boolean isConnecting) {
 		setProgressBarIndeterminateVisibility(isConnecting);
-		if (!isConnected()) {
-			clearRoster();
-		}
 		String conn, lastStatus;
 		if (isConnecting) {
 			conn = getString(R.string.conn_connecting);
+		} else if (isConnected()) {
+			conn = getString(R.string.conn_online);
 		} else {
 			conn = getString(R.string.conn_offline);
 		}
+		setTitle(getString(R.string.conn_title, conn));
+
 		if (serviceAdapter != null && (lastStatus =
 					serviceAdapter.getConnectionStateString()) != null) {
-			conn = getString(R.string.conn_laststatus, conn, lastStatus);
-		}
-		mConnectingText.setText(conn);
+			mConnectingText.setVisibility(View.VISIBLE);
+			mConnectingText.setText(lastStatus);
+		} else
+			mConnectingText.setVisibility(View.GONE);
 	}
 
 	private void toggleConnection(MenuItem item) {
 		if (isConnected() || isConnecting()) {
+			setConnectingStatus(false);
 			(new Thread() {
 				public void run() {
 					serviceAdapter.disconnect();
 					stopService(xmppServiceIntent);
 				}
 			}).start();
-
-			clearRoster();
 
 		} else {
 			setConnectingStatus(true);
@@ -401,7 +402,6 @@ public class MainWindow extends GenericExpandableListActivity {
 		if (rosterListAdapter != null) {
 			rosterListAdapter.notifyDataSetChanged();
 		}
-		mConnectingText.setText(R.string.conn_offline);
 	}
 
 	private void registerXMPPService() {
