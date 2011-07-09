9031b03e0c232139c6755ae38da1e8eae73bfbff
Michael Kanis <mks@skweez.net>
2011-07-09 15:36:41 +0200

    Update home icon according to the status that is set.

--- src/org/yaxim/androidclient/util/StatusMode.java
+++ src/org/yaxim/androidclient/util/StatusMode.java
@@ -3,12 +3,12 @@ package org.yaxim.androidclient.util;
 import org.yaxim.androidclient.R;
 
 public enum StatusMode {
-	offline(R.string.status_offline, R.drawable.ic_status_available),
-	dnd(R.string.status_dnd, R.drawable.ic_status_available),
-	xa(R.string.status_xa, R.drawable.ic_status_available),
-	away(R.string.status_away, R.drawable.ic_status_available),
+	offline(R.string.status_offline, R.drawable.ic_status_offline),
+	dnd(R.string.status_dnd, R.drawable.ic_status_dnd),
+	xa(R.string.status_xa, R.drawable.ic_status_xa),
+	away(R.string.status_away, R.drawable.ic_status_away),
 	available(R.string.status_available, R.drawable.ic_status_available),
-	chat(R.string.status_chat, R.drawable.ic_status_available);
+	chat(R.string.status_chat, R.drawable.ic_status_chat);
 
 	private final int textId;
 	private final int drawableId;
