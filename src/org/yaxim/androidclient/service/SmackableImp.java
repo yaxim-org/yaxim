yaxim-org/yaxim

commit 066bab65df7703a7a7e10c65c80af8083bd6ebc9

remove deprecated RosterItem and (list based) ExpandableRosterAdapter

src/org/yaxim/androidclient/service/SmackableImp.java
@@ -28,7 +28,6 @@ import org.jivesoftware.smack.packet.Presence;
 import org.jivesoftware.smack.packet.Presence.Mode;
 import org.jivesoftware.smack.util.StringUtils;
 import org.yaxim.androidclient.data.ChatProvider;
-import org.yaxim.androidclient.data.RosterItem;
 import org.yaxim.androidclient.data.RosterProvider;
 import org.yaxim.androidclient.data.YaximConfiguration;
 import org.yaxim.androidclient.data.ChatProvider.ChatConstants;
@@ -66,7 +65,6 @@ public class SmackableImp implements Smackable {
 	private RosterListener mRosterListener;
 	private PacketListener mPacketListener;
 
-	private final ConcurrentHashMap<String, ConcurrentHashMap<String, RosterItem>> mRosterItemsByGroup = new ConcurrentHashMap<String, ConcurrentHashMap<String, RosterItem>>();
 	private final ContentResolver mContentResolver;
 
 	public SmackableImp(YaximConfiguration config,
@@ -138,7 +136,6 @@ public class SmackableImp implements Smackable {
 	public void renameRosterGroup(String group, String newGroup) {
 		mRoster = mXMPPConnection.getRoster();
 		RosterGroup groupToRename = mRoster.getGroup(group);
-		mRosterItemsByGroup.remove(group);
 		groupToRename.setName(newGroup);
 	}
 
@@ -235,9 +232,6 @@ public class SmackableImp implements Smackable {
 			RosterEntry rosterEntry) throws YaximXMPPException {
 		try {
 			group.removeEntry(rosterEntry);
-			if (group.getEntries().size() < 1) {
-				mRosterItemsByGroup.remove(group.getName());
-			}
 		} catch (XMPPException e) {
 			throw new YaximXMPPException(e.getLocalizedMessage());
 		}
@@ -270,88 +264,10 @@ public class SmackableImp implements Smackable {
 		mRoster = mXMPPConnection.getRoster();
 		Collection<RosterEntry> rosterEntries = mRoster.getEntries();
 		for (RosterEntry rosterEntry : rosterEntries) {
-			setRosterEntry(rosterEntry);
 			addRosterEntryToDB(rosterEntry);
 		}
-		mServiceCallBack.rosterChanged();
 	}
 
-	private void setRosterEntry(RosterEntry rosterEntry) {
-		String groupName = getGroup(rosterEntry.getGroups());
-		ConcurrentMap<String, RosterItem> entryMap = getEntryMapForGroup(groupName);
-		RosterItem rosterItem = getRosterItemForRosterEntry(rosterEntry);
-		entryMap.put(rosterItem.jabberID, rosterItem);
-	}
-
-	private void unSetRosterEntry(String jabberID) {
-		Set<Entry<String, ConcurrentHashMap<String, RosterItem>>> groupMaps = mRosterItemsByGroup
-				.entrySet();
-
-		for (Entry<String, ConcurrentHashMap<String, RosterItem>> entry : groupMaps) {
-			ConcurrentHashMap<String, RosterItem> entryMap = entry.getValue();
-			String groupName = entry.getKey();
-
-			if (entryMap.containsKey(jabberID)) {
-				entryMap.remove(jabberID);
-			}
-			if (entryMap.size() < 1) {
-				mRosterItemsByGroup.remove(groupName);
-			}
-		}
-	}
-
-	private ConcurrentMap<String, RosterItem> getEntryMapForGroup(
-			String groupName) {
-		ConcurrentHashMap<String, RosterItem> tmpItemList;
-		if (mRosterItemsByGroup.containsKey(groupName))
-			return mRosterItemsByGroup.get(groupName);
-		else {
-			tmpItemList = new ConcurrentHashMap<String, RosterItem>();
-			mRosterItemsByGroup.put(groupName, tmpItemList);
-		}
-		return tmpItemList;
-	}
-
-	public ArrayList<RosterItem> getRosterEntriesByGroup(String group) {
-		ArrayList<RosterItem> groupItems = new ArrayList<RosterItem>();
-
-		ConcurrentHashMap<String, RosterItem> rosterItemMap = mRosterItemsByGroup
-				.get(group);
-
-		if (rosterItemMap != null) {
-			groupItems.addAll(rosterItemMap.values());
-			Collections.sort(groupItems);
-		}
-
-		return groupItems;
-	}
-
-	private RosterItem getRosterItemForRosterEntry(RosterEntry entry) {
-		String jabberID = entry.getUser();
-		String userName = getName(entry);
-		Presence userPresence = mRoster.getPresence(entry.getUser());
-		StatusMode userStatus = getStatus(userPresence);
-		String userStatusMessage = userPresence.getStatus();
-		String userGroups = getGroup(entry.getGroups());
-
-
-		debugLog("getRosterItemForRosterEntry(): " + jabberID + " -> " + userName);
-		return new RosterItem(jabberID, userName, userStatus,
-				userStatusMessage, userGroups);
-	}
-
-	public ArrayList<String> getRosterGroups() {
-		ArrayList<String> rosterGroups = new ArrayList<String>(
-				mRosterItemsByGroup.keySet());
-		Collections.sort(rosterGroups, new Comparator<String>() {
-			public int compare(String group1, String group2) {
-				if (group1.equals(AdapterConstants.EMPTY_GROUP))
-					return -1;
-				return group1.toLowerCase().compareTo(group2.toLowerCase());
-			}
-		});
-		return rosterGroups;
-	}
 
 	public void setStatusFromConfig() {
 		Presence presence = new Presence(Presence.Type.available);
@@ -419,7 +335,6 @@ public class SmackableImp implements Smackable {
 		if (mXMPPConnection.isConnected()) {
 			mXMPPConnection.disconnect();
 		}
-		mRosterItemsByGroup.clear();
 		setStatusOffline();
 		this.mServiceCallBack = null;
 	}
@@ -450,7 +365,6 @@ public class SmackableImp implements Smackable {
 
 				for (String entry : entries) {
 					RosterEntry rosterEntry = mRoster.getEntry(entry);
-					setRosterEntry(rosterEntry);
 					addRosterEntryToDB(rosterEntry);
 				}
 				mServiceCallBack.rosterChanged();
@@ -461,7 +375,6 @@ public class SmackableImp implements Smackable {
 
 				for (String entry : entries) {
 					RosterEntry rosterEntry = mRoster.getEntry(entry);
-					unSetRosterEntry(entry);
 					deleteRosterEntryFromDB(entry);
 				}
 				mServiceCallBack.rosterChanged();
@@ -472,8 +385,6 @@ public class SmackableImp implements Smackable {
 
 				for (String entry : entries) {
 					RosterEntry rosterEntry = mRoster.getEntry(entry);
-					unSetRosterEntry(rosterEntry.getUser());
-					setRosterEntry(rosterEntry);
 					updateRosterEntryInDB(rosterEntry);
 				}
 				mServiceCallBack.rosterChanged();
@@ -484,8 +395,6 @@ public class SmackableImp implements Smackable {
 
 				String jabberID = getJabberID(presence.getFrom());
 				RosterEntry rosterEntry = mRoster.getEntry(jabberID);
-				setRosterEntry(rosterEntry);
-				mServiceCallBack.rosterChanged();
 				updateOrInsertRosterEntryToDB(rosterEntry);
 			}
 		};

src/org/yaxim/androidclient/IXMPPRosterCallback.aidl
@@ -7,5 +7,4 @@ package org.yaxim.androidclient;
 interface IXMPPRosterCallback {
 	void connectionSuccessful();
 	void connectionFailed(boolean willReconnect);
-	void rosterChanged();
 }

src/org/yaxim/androidclient/MainWindow.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import org.yaxim.androidclient.data.RosterItem;
 import org.yaxim.androidclient.data.RosterProvider;
 import org.yaxim.androidclient.dialogs.AddRosterItemDialog;
 import org.yaxim.androidclient.dialogs.ChangeStatusDialog;
@@ -15,7 +14,6 @@ import org.yaxim.androidclient.preferences.MainPrefs;
 import org.yaxim.androidclient.service.XMPPService;
 import org.yaxim.androidclient.util.AdapterConstants;
 import org.yaxim.androidclient.util.ConnectionState;
-import org.yaxim.androidclient.util.ExpandableRosterAdapter;
 import org.yaxim.androidclient.util.PreferenceConstants;
 import org.yaxim.androidclient.util.StatusMode;
 

src/org/yaxim/androidclient/XMPPRosterServiceAdapter.java
@@ -2,7 +2,6 @@ package org.yaxim.androidclient;
 
 import java.util.List;
 
-import org.yaxim.androidclient.data.RosterItem;
 import org.yaxim.androidclient.util.ConnectionState;
 import org.yaxim.androidclient.util.StatusMode;
 
@@ -78,16 +77,6 @@ public class XMPPRosterServiceAdapter {
 		}
 	}
 	
-	public List<String> getRosterGroups() {
-		List<String> rosterGroups = null;
-		try {
-			rosterGroups = xmppServiceStub.getRosterGroups();
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
-		return rosterGroups;
-	}
-	
 	public void disconnect() {
 		try {
 			xmppServiceStub.disconnect();
@@ -104,16 +93,6 @@ public class XMPPRosterServiceAdapter {
 		}
 	}
 
-	public List<RosterItem> getGroupItems(String group) {
-		List<RosterItem> itemsInRosterGroup = null;
-		try {
-			itemsInRosterGroup = xmppServiceStub.getRosterEntriesByGroup(group);
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
-		return itemsInRosterGroup;
-	}
-	
 	public void registerUICallback(IXMPPRosterCallback uiCallback) {
 		try {
 			xmppServiceStub.registerRosterCallback(uiCallback);

src/org/yaxim/androidclient/data/RosterItem.aidl
@@ -1,3 +1,0 @@
-package org.yaxim.androidclient.data;
-
-parcelable RosterItem;

src/org/yaxim/androidclient/data/RosterItem.java
@@ -1,76 +1,0 @@
-package org.yaxim.androidclient.data;
-
-import org.yaxim.androidclient.util.StatusMode;
-
-import android.os.Parcel;
-import android.os.Parcelable;
-
-public class RosterItem implements Parcelable, Comparable<RosterItem> {
-
-	public final String jabberID;
-	public final String screenName;
-	public final String statusMessage;
-	public final String group;
-
-	private final String statusMode;
-
-	public RosterItem(String jabberID, String screenName,
-			StatusMode statusMode, String statusMessage, String group) {
-		this.jabberID = jabberID;
-		this.screenName = screenName;
-		this.statusMode = statusMode.name();
-		this.statusMessage = statusMessage;
-		this.group = group;
-	}
-
-	private RosterItem(Parcel in) {
-		this.jabberID = in.readString();
-		this.screenName = in.readString();
-		this.statusMode = in.readString();
-		this.statusMessage = in.readString();
-		this.group = in.readString();
-	}
-
-	@SuppressWarnings("unchecked")
-	public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
-
-		public RosterItem createFromParcel(Parcel in) {
-			return new RosterItem(in);
-		}
-
-		public RosterItem[] newArray(int size) {
-			return new RosterItem[size];
-
-		}
-	};
-
-	public void writeToParcel(Parcel out, int flags) {
-		out.writeString(jabberID);
-		out.writeString(screenName);
-		out.writeString(statusMode);
-		out.writeString(statusMessage);
-		out.writeString(group);
-	}
-
-	public StatusMode getStatusMode() {
-		return StatusMode.valueOf(statusMode);
-	}
-
-	public int describeContents() {
-		return 0;
-	}
-
-	public String toString() {
-		return this.screenName;
-	}
-
-	public int compareTo(RosterItem that) {
-		if (this.getStatusMode() == that.getStatusMode()) {
-			return this.screenName.compareTo(that.screenName);
-		}
-
-		// Reversed ordinality order → Free for chat first, offline last
-		return that.getStatusMode().compareTo(getStatusMode());
-	}
-
-}

src/org/yaxim/androidclient/service/IXMPPRosterService.aidl
@@ -5,7 +5,6 @@ package org.yaxim.androidclient.service;
 */
 
 import org.yaxim.androidclient.IXMPPRosterCallback;
-import org.yaxim.androidclient.data.RosterItem;
 
 interface IXMPPRosterService {
 	/* hack: use int because enums are not trivially parcellable */
@@ -24,9 +23,6 @@ interface IXMPPRosterService {
 	void requestAuthorizationForRosterItem(String user);
 	void renameRosterItem(String user, String newName);
 	void moveRosterItemToGroup(String user, String group);
-	
-	List<RosterItem> getRosterEntriesByGroup(String group);
-	List<String> getRosterGroups();
 	
 	/* callback methods */
 	

src/org/yaxim/androidclient/service/Smackable.java
@@ -2,7 +2,6 @@ package org.yaxim.androidclient.service;
 
 import java.util.ArrayList;
 
-import org.yaxim.androidclient.data.RosterItem;
 import org.yaxim.androidclient.exceptions.YaximXMPPException;
 import org.yaxim.androidclient.util.StatusMode;
 
@@ -25,9 +24,6 @@ public interface Smackable {
 	
 	void registerCallback(XMPPServiceCallback callBack);
 	void unRegisterCallback();
-	
-	ArrayList<RosterItem> getRosterEntriesByGroup(String group);
-	ArrayList<String> getRosterGroups();
 	
 	String getNameForJID(String jid);
 }

commit 7dbdfd7d8522d09d6f18a7e406b07796e16a3067

re-add missing service callback (broken in 066bab65)

src/org/yaxim/androidclient/service/SmackableImp.java
@@ -396,6 +396,7 @@ public class SmackableImp implements Smackable {
 				String jabberID = getJabberID(presence.getFrom());
 				RosterEntry rosterEntry = mRoster.getEntry(jabberID);
 				updateOrInsertRosterEntryToDB(rosterEntry);
+				mServiceCallBack.rosterChanged();
 			}
 		};
 		mRoster.addRosterListener(mRosterListener);