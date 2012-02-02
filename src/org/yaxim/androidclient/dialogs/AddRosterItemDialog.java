a98d42eebf478e61e9174be1d125772d62681188
Georg Lukas <georg@op-co.de>
2012-02-02 23:32:14 +0100

    AlertDialog'ize add contact dialog

--- src/org/yaxim/androidclient/dialogs/AddRosterItemDialog.java
+++ src/org/yaxim/androidclient/dialogs/AddRosterItemDialog.java
@@ -9,92 +9,73 @@ import org.yaxim.androidclient.util.AdapterConstants;
 import org.yaxim.androidclient.util.XMPPHelper;
 import org.yaxim.androidclient.MainWindow;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import org.yaxim.androidclient.R;
 
-public class AddRosterItemDialog extends GenericDialog implements
-		OnClickListener, TextWatcher {
+public class AddRosterItemDialog extends AlertDialog implements
+		DialogInterface.OnClickListener, TextWatcher {
 
 	private MainWindow mMainWindow;
+	private XMPPRosterServiceAdapter mServiceAdapter;
 
-	private Button cancelButton;
 	private Button okButton;
 	private EditText userInputField;
 	private EditText aliasInputField;
 	private GroupNameView mGroupNameView;
 
-	public AddRosterItemDialog(Context mainWindow,
+	public AddRosterItemDialog(MainWindow mainWindow,
 			XMPPRosterServiceAdapter serviceAdapter) {
-		super(mainWindow, serviceAdapter);
-		mMainWindow = (MainWindow)mainWindow;
+		super(mainWindow);
+		mMainWindow = mainWindow;
+		mServiceAdapter = serviceAdapter;
+
+		setTitle(R.string.addFriend_Title);
+
+		LayoutInflater inflater = (LayoutInflater) mainWindow
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View group = inflater.inflate(R.layout.addrosteritemdialog, null, false);
+		setView(group);
+
+		userInputField = (EditText)group.findViewById(R.id.AddContact_EditTextField);
+		userInputField.addTextChangedListener(this);
+		aliasInputField = (EditText)group.findViewById(R.id.AddContactAlias_EditTextField);
+
+		mGroupNameView = (GroupNameView)group.findViewById(R.id.AddRosterItem_GroupName);
+		mGroupNameView.setGroupList(mMainWindow.getRosterGroups());
+
+		setButton(BUTTON_POSITIVE, mainWindow.getString(android.R.string.ok), this);
+		setButton(BUTTON_NEGATIVE, mainWindow.getString(android.R.string.cancel),
+				(DialogInterface.OnClickListener)null);
+
 	}
 
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
-		setContentView(R.layout.addrosteritemdialog);
-		setTitle(R.string.addFriend_Title);
 
-		setUserInputField();
-		setAliasInputField();
-		createAndSetGroupSpinnerAdapter();
-		setOkButton();
-		setCancelButton();
-
+		okButton = getButton(BUTTON_POSITIVE);
+		okButton.setEnabled(false);
 	}
 
-	private void createAndSetGroupSpinnerAdapter() {
-		mGroupNameView = (GroupNameView)findViewById(R.id.AddRosterItem_GroupName);
-		mGroupNameView.setGroupList(mMainWindow.getRosterGroups());
-	}
-
-	private void setOkButton() {
-		okButton = (Button) findViewById(R.id.AddContact_OkButton);
-	}
-
-	private void setUserInputField() {
-		userInputField = (EditText) findViewById(R.id.AddContact_EditTextField);
-		userInputField.addTextChangedListener(this);
-	}
-
-	private void setAliasInputField() {
-		aliasInputField = (EditText) findViewById(R.id.AddContactAlias_EditTextField);
-	}
-
-	private void setCancelButton() {
-		cancelButton = (Button) findViewById(R.id.AddContact_CancelButton);
-		cancelButton.setOnClickListener(this);
-	}
-
-	public void onClick(View view) {
-
-		switch (view.getId()) {
-
-		case R.id.AddContact_CancelButton:
-			cancel();
-			break;
-
-		case R.id.AddContact_OkButton:
-			serviceAdapter.addRosterItem(userInputField.getText()
-					.toString(), aliasInputField.getText().toString(),
-					mGroupNameView.getGroupName());
-			cancel();
-			break;
-		}
+	public void onClick(DialogInterface dialog, int which) {
+		mServiceAdapter.addRosterItem(userInputField.getText()
+				.toString(), aliasInputField.getText().toString(),
+				mGroupNameView.getGroupName());
 	}
 
 	public void afterTextChanged(Editable s) {
 		try {
 			XMPPHelper.verifyJabberID(s);
 			okButton.setEnabled(true);
-			okButton.setOnClickListener(this);
 			userInputField.setTextColor(Color.DKGRAY);
 		} catch (YaximXMPPAdressMalformedException e) {
 			okButton.setEnabled(false);
