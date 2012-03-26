yaxim-org/yaxim

commit f26f0b95411dc6887bf3e0657cb175283310b4da

Move export-icons.sh into toplevel folder
The Eclipse Android plugin stumbles over this file in asset-graphics,
gets grumpy and silently refuses to process any aidl file in revenge.
Resolve this by moving the script out of the way.

Signed-off-by: Jan Kiszka <jan.kiszka@web.de>

export-icons.sh
@@ -6,7 +6,7 @@ svg2png() {
 	width=$2
 	height=$3
 	dest=$4
-	inkscape --export-png=../res/$dest/$basename.png --export-width=$width --export-height=$height --export-background-opacity=0,0 -C -z $basename.svg
+	inkscape --export-png=res/$dest/$basename.png --export-width=$width --export-height=$height --export-background-opacity=0,0 -C -z $basename.svg
 }
 
 svg2icon() {
@@ -38,15 +38,15 @@ svg2sbar() {
 }
 
 # convert icon
-svg2icon icon
+svg2icon asset-graphics/icon
 
 # convert statusbar notification icon
-svg2sbar sb_message
+svg2sbar asset-graphics/sb_message
 
 # convert status
 # convert paw status
-for file in `ls ic_*.svg`
+for file in `ls asset-graphics/ic_*.svg`
 do
 	basename=`basename $file .svg`
-	svg2status $basename
+	svg2status asset-graphics/$basename
 done

commit 65588abf303e96b0ad5a4d8b86a28ffc26082fb0

Fix export-icons script
Paths were broken after f26f0b9541.

Signed-off-by: Jan Kiszka <jan.kiszka@web.de>

export-icons.sh
@@ -6,7 +6,7 @@ svg2png() {
 	width=$2
 	height=$3
 	dest=$4
-	inkscape --export-png=res/$dest/$basename.png --export-width=$width --export-height=$height --export-background-opacity=0,0 -C -z $basename.svg
+	inkscape --export-png=res/$dest/$basename.png --export-width=$width --export-height=$height --export-background-opacity=0,0 -C -z asset-graphics/$basename.svg
 }
 
 svg2icon() {
@@ -38,15 +38,15 @@ svg2sbar() {
 }
 
 # convert icon
-svg2icon asset-graphics/icon
+svg2icon icon
 
 # convert statusbar notification icon
-svg2sbar asset-graphics/sb_message
+svg2sbar sb_message
 
 # convert status
 # convert paw status
 for file in `ls asset-graphics/ic_*.svg`
 do
 	basename=`basename $file .svg`
-	svg2status asset-graphics/$basename
+	svg2status $basename
 done
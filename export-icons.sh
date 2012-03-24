65588abf303e96b0ad5a4d8b86a28ffc26082fb0
Jan Kiszka <jan.kiszka@web.de>
2012-03-24 18:28:56 +0100

    Fix export-icons script
    Paths were broken after f26f0b9541.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@web.de>

--- export-icons.sh
+++ export-icons.sh
@@ -6,7 +6,7 @@ svg2png() {
 	width=$2
 	height=$3
 	dest=$4
-	inkscape --export-png=res/$dest/$basename.png --export-width=$width --export-height=$height --export-background-opacity=0,0 -C -z $basename.svg
+	inkscape --export-png=res/$dest/$basename.png --export-width=$width --export-height=$height --export-background-opacity=0,0 -C -z asset-graphics/$basename.svg
 }
 
 svg2icon() {
@@ -38,15 +38,15 @@ svg2sbar() {
 }
 
 # convert icon
-svg2icon asset-graphics/icon
+svg2icon icon
 
 # convert statusbar notification icon
-svg2sbar asset-graphics/sb_message
+svg2sbar sb_message
 
 # convert status
 # convert paw status
 for file in `ls asset-graphics/ic_*.svg`
 do
 	basename=`basename $file .svg`
-	svg2status asset-graphics/$basename
+	svg2status $basename
 done
