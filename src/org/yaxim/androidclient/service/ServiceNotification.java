5e0d28d2ff68ad3ec87a5f1c57e98e5f53624f4f
Georg Lukas <georg@op-co.de>
2012-04-01 15:38:51 +0200

    use reflection for setForeground on 3.x SDK

--- src/org/yaxim/androidclient/service/ServiceNotification.java
+++ src/org/yaxim/androidclient/service/ServiceNotification.java
@@ -2,24 +2,24 @@ package org.yaxim.androidclient.service;
 
 import android.util.Log;
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
+import android.os.Build;
 
 // Code to make a Service stay in the foreground from
 // http://devtcg.blogspot.com/2009/12/gracefully-supporting-multiple-android.html
 abstract class ServiceNotification {
 	public static ServiceNotification getInstance() {
-		/*
 		if (Integer.parseInt(Build.VERSION.SDK) <= 4)
 			return PreEclair.Holder.sInstance;
 		else
-		*/
 			return EclairAndBeyond.Holder.sInstance;
 	}
 
 	public abstract void showNotification(Service context, int id, Notification notification);
 	public abstract void hideNotification(Service context, int id);
 
-	/*
 	private static class PreEclair extends ServiceNotification {
 		private static class Holder {
 			private static final PreEclair sInstance = new PreEclair();
@@ -28,15 +28,30 @@ abstract class ServiceNotification {
 			return (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
 		}
 		public void showNotification(Service context, int id, Notification n) {
-			context.setForeground(true);
+			setForeground(context, true);
 			getNotificationManager(context).notify(id, n);
 		}
 		public void hideNotification(Service context, int id) {
-			context.setForeground(false);
+			setForeground(context, false);
 			getNotificationManager(context).cancel(id);
 		}
+
+		/**
+		 * Will call the "setForeground" method if available using reflection,
+		 * since the method has been removed completely in Android 3.0 and
+		 * above.
+		 * @param foreground should service run as a foreground service?
+		 *
+		 * from https://gist.github.com/857686
+		 */
+		public void setForeground(Context ctx, boolean foreground) {
+			try {
+				ctx.getClass().getMethod("setForeground", boolean.class).invoke(this, foreground);
+			} catch (Exception e) {
+				// Too bad!
+			}
+		}
 	}
-	*/
 
 	private static class EclairAndBeyond extends ServiceNotification {
 		private static class Holder {
