yaxim-org/yaxim

commit 7e680508ce59988564101e04e539fae1d7f5c38d

Signal unread messages in roster view
If some contact has unread messages pending, tag the roster entry with a
speech bubble.

Signed-off-by: Jan Kiszka <jan.kiszka@web.de>

src/org/yaxim/androidclient/MainWindow.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.yaxim.androidclient.data.ChatProvider;
 import org.yaxim.androidclient.data.RosterProvider;
 import org.yaxim.androidclient.data.YaximConfiguration;
+import org.yaxim.androidclient.data.ChatProvider.ChatConstants;
 import org.yaxim.androidclient.dialogs.AddRosterItemDialog;
 import org.yaxim.androidclient.dialogs.ChangeStatusDialog;
 import org.yaxim.androidclient.dialogs.FirstStartDialog;
@@ -82,6 +83,7 @@ public class MainWindow extends SherlockExpandableListActivity {
 	private StatusMode mStatusMode;
 
 	private ContentObserver mRosterObserver = new RosterObserver();
+	private ContentObserver mChatObserver = new ChatObserver();
 	private HashMap<String, Boolean> mGroupsExpanded = new HashMap<String, Boolean>();
 
 	private ActionBar actionBar;
@@ -106,6 +108,8 @@ public class MainWindow extends SherlockExpandableListActivity {
 		showFirstStartUpDialogIfPrefsEmpty();
 		getContentResolver().registerContentObserver(RosterProvider.GROUPS_URI,
 				true, mRosterObserver);
+		getContentResolver().registerContentObserver(ChatProvider.CONTENT_URI,
+				true, mChatObserver);
 		registerXMPPService();
 		createUICallback();
 		setupContenView();
@@ -118,6 +122,7 @@ public class MainWindow extends SherlockExpandableListActivity {
 	public void onDestroy() {
 		super.onDestroy();
 		getContentResolver().unregisterContentObserver(mRosterObserver);
+		getContentResolver().unregisterContentObserver(mChatObserver);
 	}
 
 	public int getStatusActionIcon() {
@@ -1005,6 +1010,17 @@ public class MainWindow extends SherlockExpandableListActivity {
 			TextView statusmsg = (TextView)view.findViewById(R.id.roster_statusmsg);
 			boolean hasStatus = statusmsg.getText() != null && statusmsg.getText().length() > 0;
 			statusmsg.setVisibility(hasStatus ? View.VISIBLE : View.GONE);
+
+			int JIDIdx = cursor.getColumnIndex(RosterProvider.RosterConstants.JID);
+			String selection = ChatConstants.JID + " = '" + cursor.getString(JIDIdx) + "' AND " +
+					ChatConstants.FROM_ME + " = 0 AND " +
+					ChatConstants.DELIVERY_STATUS + " = " + ChatConstants.DS_NEW;
+			Cursor msgcursor = getContentResolver().query(ChatProvider.CONTENT_URI,
+					new String[] { ChatConstants.PACKET_ID }, selection, null, null);
+			TextView unreadmsg = (TextView)view.findViewById(R.id.roster_unreadmsg_cnt);
+			unreadmsg.setText(String.valueOf(msgcursor.getCount()));
+			unreadmsg.setVisibility(msgcursor.getCount() > 0 ? View.VISIBLE : View.GONE);
+			unreadmsg.bringToFront();
 		}
 
 		 protected void setViewImage(ImageView v, String value) {
@@ -1030,6 +1046,15 @@ public class MainWindow extends SherlockExpandableListActivity {
 					public void run() {
 						restoreGroupsExpanded();
 					}}, 100);
+		}
+	}
+
+	private class ChatObserver extends ContentObserver {
+		public ChatObserver() {
+			super(mainHandler);
+		}
+		public void onChange(boolean selfChange) {
+			updateRoster();
 		}
 	}
 }

res/drawable/count_bcg.xml
@@ -1,0 +1,6 @@
+<?xml version="1.0" encoding="utf-8"?>
+<shape xmlns:android="http://schemas.android.com/apk/res/android"
+       android:shape="rectangle">
+    <solid android:color="#FFE73C18" />
+    <corners android:radius="4dp" />
+</shape>

res/layout/mainchild_row.xml
@@ -3,10 +3,14 @@
 	android:orientation="horizontal" android:layout_width="fill_parent"
 	android:layout_height="wrap_content" android:gravity="center_vertical">
 
-	<ImageView android:id="@+id/roster_icon"
-		android:layout_width="wrap_content" android:layout_height="wrap_content"
-		android:layout_marginRight="4dp" android:longClickable="false"
-		android:paddingLeft="15sp" />
+	<RelativeLayout android:layout_width="52dp" android:layout_height="wrap_content">
+		<TextView android:id="@+id/roster_unreadmsg_cnt"
+			style="@style/RosterMessageCounter" />
+	    <ImageView android:id="@+id/roster_icon"
+			android:layout_width="wrap_content" android:layout_height="wrap_content"
+			android:layout_marginRight="4dp" android:longClickable="false"
+			android:paddingLeft="15sp" />
+	</RelativeLayout>
 
 	<LinearLayout android:layout_width="fill_parent"
 		android:layout_height="wrap_content" android:orientation="vertical"

res/values/styles.xml
@@ -33,4 +33,27 @@
 		<item name="android:background">#f88</item>
 		<item name="android:textColor">#400</item>
 	</style>
+    <style name="RosterMessageCounter">
+        <item name="android:visibility">gone</item>
+        <item name="android:layout_width">wrap_content</item>
+        <item name="android:layout_height">wrap_content</item>
+        <item name="android:layout_gravity">center_horizontal</item>
+        <item name="android:layout_alignParentBottom">true</item>
+        <item name="android:layout_alignParentRight">true</item>
+        <item name="android:layout_marginRight">4dip</item>
+        <item name="android:layout_marginLeft">4dip</item>
+        <item name="android:gravity">center</item>
+        <item name="android:paddingTop">2dip</item>
+        <item name="android:paddingBottom">2dip</item>
+        <item name="android:paddingLeft">5dip</item>
+        <item name="android:paddingRight">5dip</item>
+        <item name="android:singleLine">true</item>
+        <item name="android:lines">1</item>
+        <item name="android:ellipsize">marquee</item>
+        <item name="android:textSize">11dip</item>
+        <item name="android:textColor">#FFFFFFFF</item>
+        <item name="android:background">@drawable/count_bcg</item>
+        <item name="android:text"></item>
+    </style>
+
 </resources>

commit 56f6d5207239a1b96b070b3b0d4e0cf669cf0d0e

fix cursor leak introduced in 7e680508

src/org/yaxim/androidclient/MainWindow.java
@@ -1021,6 +1021,7 @@ public class MainWindow extends SherlockExpandableListActivity {
 			unreadmsg.setText(String.valueOf(msgcursor.getCount()));
 			unreadmsg.setVisibility(msgcursor.getCount() > 0 ? View.VISIBLE : View.GONE);
 			unreadmsg.bringToFront();
+			msgcursor.close();
 		}
 
 		 protected void setViewImage(ImageView v, String value) {