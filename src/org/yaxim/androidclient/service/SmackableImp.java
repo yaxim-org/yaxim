f43956adc168bdf36c1831377ae4ce0194565c14
Jan Kiszka <jan.kiszka@web.de>
2012-04-09 23:09:46 +0200

    Perform string content comparison in SmackableImp::getName
    This may have worked by accident if the invoked methods returned the
    same static string objects, but better replace this with a proper check.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@web.de>

--- src/org/yaxim/androidclient/service/SmackableImp.java
+++ src/org/yaxim/androidclient/service/SmackableImp.java
@@ -665,11 +665,11 @@ public class SmackableImp implements Smackable {
 
 	private String getName(RosterEntry rosterEntry) {
 		String name = rosterEntry.getName();
-		if (name != null && name != "") {
+		if (name != null && name.length() > 0) {
 			return name;
 		}
 		name = StringUtils.parseName(rosterEntry.getUser());
-		if (name != "") {
+		if (name.length() > 0) {
 			return name;
 		}
 		return rosterEntry.getUser();
