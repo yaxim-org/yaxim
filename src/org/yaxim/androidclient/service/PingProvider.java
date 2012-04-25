470ada4d32bfba57905d9d44f98e17db0e1c5414
tuxmobil <tuxmobil@gmx.de>
2012-04-25 22:29:28 +0200

    Additional smack classes for XEP-0199 implementation

---
+++ src/org/yaxim/androidclient/service/PingProvider.java
@@ -0,0 +1,14 @@
+package org.yaxim.androidclient.service;
+
+import org.jivesoftware.smack.packet.IQ;
+import org.jivesoftware.smack.provider.IQProvider;
+import org.xmlpull.v1.XmlPullParser;
+
+public class PingProvider implements IQProvider{
+
+	@Override
+	public IQ parseIQ(XmlPullParser arg0) throws Exception {
+		return new Ping();
+	}
+
+}
