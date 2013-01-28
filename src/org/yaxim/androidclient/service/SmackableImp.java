67788635586a5428ded1ebe0d3358912bc293548
Georg Lukas <georg@op-co.de>
2013-01-28 21:22:18 +0100

    catch 0.8.5 crash: race between doConnect and unRegisterCallback

--- src/org/yaxim/androidclient/service/SmackableImp.java
+++ src/org/yaxim/androidclient/service/SmackableImp.java
@@ -167,6 +167,13 @@ public class SmackableImp implements Smackable {
 			registerPingListener();
 			registerPongListener();
 			sendOfflineMessages();
+			if (mServiceCallBack == null) {
+				// sometimes we get disconnected while not yet quite connected.
+				// bail out if this is the case
+				debugLog("doConnect: mServiceCallBack is null, aborting connection...");
+				mXMPPConnection.disconnect();
+				return false;
+			}
 			// we need to "ping" the service to let it know we are actually
 			// connected, even when no roster entries will come in
 			mServiceCallBack.rosterChanged();
