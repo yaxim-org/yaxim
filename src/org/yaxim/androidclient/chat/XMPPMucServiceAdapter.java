8e2ca5aaf7830d2987112dd65f118b09e2c6182b
Dario Ernst <daddel9@nebuk.de>
2013-02-28 18:35:02 +0100

    adding files is fun!

---
+++ src/org/yaxim/androidclient/chat/XMPPMucServiceAdapter.java
@@ -0,0 +1,31 @@
+package org.yaxim.androidclient.chat;
+
+import android.os.RemoteException;
+import android.util.Log;
+import org.yaxim.androidclient.service.IXMPPChatService;
+import org.yaxim.androidclient.service.IXMPPMucService;
+
+public class XMPPMucServiceAdapter {
+
+	private static final String TAG = "yaxim.XMPPCSAdapter";
+	private IXMPPMucService xmppServiceStub;
+	private String jabberID;
+
+	public XMPPMucServiceAdapter(IXMPPMucService xmppServiceStub,
+			String jabberID) {
+		Log.i(TAG, "New XMPPMucServiceAdapter construced");
+		this.xmppServiceStub = xmppServiceStub;
+		this.jabberID = jabberID;
+	}
+	
+	public boolean isRoom(String jid) {
+		try {
+			Log.d(TAG, "called isRoom with jid "+jid+" and got response "+xmppServiceStub.isRoom(jid));
+			return xmppServiceStub.isRoom(jid);
+		} catch (RemoteException e) {
+			Log.e(TAG, "Caught RemoteException: "+e.getMessage());
+			return false;
+		}
+	}
+
+}
