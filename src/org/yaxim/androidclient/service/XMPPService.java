aa88f907a05caed97bc4ea5ad2373052a38d8d57
Georg Lukas <georg@op-co.de>
2013-06-13 17:45:31 +0200

    hack: try to re-connect in ping handler

--- src/org/yaxim/androidclient/service/XMPPService.java
+++ src/org/yaxim/androidclient/service/XMPPService.java
@@ -151,11 +151,11 @@ public class XMPPService extends GenericService {
 				return START_STICKY;
 			} else
 			if ("ping".equals(intent.getAction())) {
-				if (mSmackable != null) {
+				if (mSmackable != null && mSmackable.isAuthenticated()) {
 					mSmackable.sendServerPing();
 					return START_STICKY;
 				}
-				// if not yet connected, fall through to doConnect()
+				// if not authenticated, fall through to doConnect()
 			}
 		}
 		
