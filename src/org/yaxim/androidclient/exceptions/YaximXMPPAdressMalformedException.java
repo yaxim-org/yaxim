777c23ea5ec36bc1cdec18f5c638bf8e92e59b0d
Georg Lukas <georg@op-co.de>
2013-12-20 23:08:46 +0100

    backend: add IDNA verifier

--- src/org/yaxim/androidclient/exceptions/YaximXMPPAdressMalformedException.java
+++ src/org/yaxim/androidclient/exceptions/YaximXMPPAdressMalformedException.java
@@ -4,6 +4,10 @@ public class YaximXMPPAdressMalformedException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
 
+	public YaximXMPPAdressMalformedException(Throwable cause) {
+		super(cause);
+	}
+
 	public YaximXMPPAdressMalformedException(String msg) {
 		super(msg);
 	}
--- src/org/yaxim/androidclient/util/XMPPHelper.java
+++ src/org/yaxim/androidclient/util/XMPPHelper.java
@@ -9,28 +9,33 @@ import android.content.Context;
 import android.text.Editable;
 import android.util.TypedValue;
 
+import gnu.inet.encoding.Stringprep;
+import gnu.inet.encoding.StringprepException;
 
 public class XMPPHelper {
 
-	public static void verifyJabberID(String jid)
+	public static String verifyJabberID(String jid)
 			throws YaximXMPPAdressMalformedException {
-		if (jid != null) {
-			Pattern p = Pattern
-					.compile("(?i)[a-z0-9\\-_\\.]++@[a-z0-9\\-_]++(\\.[a-z0-9\\-_]++)++");
-			Matcher m = p.matcher(jid);
-
-			if (!m.matches()) {
+		try {
+			String parts[] = jid.split("@");
+			if (parts.length != 2 || parts[0].length() == 0 || parts[1].length() == 0)
 				throw new YaximXMPPAdressMalformedException(
 						"Configured Jabber-ID is incorrect!");
-			}
-		} else {
+			StringBuilder sb = new StringBuilder();
+			sb.append(Stringprep.nodeprep(parts[0]));
+			sb.append("@");
+			sb.append(Stringprep.nameprep(parts[1]));
+			return sb.toString();
+		} catch (StringprepException spe) {
+			throw new YaximXMPPAdressMalformedException(spe);
+		} catch (NullPointerException e) {
 			throw new YaximXMPPAdressMalformedException("Jabber-ID wasn't set!");
 		}
 	}
 
-	public static void verifyJabberID(Editable jid)
+	public static String verifyJabberID(Editable jid)
 			throws YaximXMPPAdressMalformedException {
-		verifyJabberID(jid.toString());
+		return verifyJabberID(jid.toString());
 	}
 	
 	public static int tryToParseInt(String value, int defVal) {
