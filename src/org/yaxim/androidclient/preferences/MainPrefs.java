a63874999ad0af7cee1af32abd504780bb8d1ad3
Georg Lukas <georg@op-co.de>
2014-02-26 12:33:40 +0100

    all activities: action bar update
    The documented mechanism for providing a fallback API on r7 devices, as
    specified by Google, does not actually work. Therefore we need to
    manually catch the options menu item and call startActivity.

--- src/org/yaxim/androidclient/preferences/MainPrefs.java
+++ src/org/yaxim/androidclient/preferences/MainPrefs.java
@@ -1,16 +1,37 @@
 package org.yaxim.androidclient.preferences;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+
+import org.yaxim.androidclient.MainWindow;
 import org.yaxim.androidclient.R;
 import org.yaxim.androidclient.YaximApplication;
 
-public class MainPrefs extends PreferenceActivity{
+public class MainPrefs extends SherlockPreferenceActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		setTheme(YaximApplication.getConfig(this).getTheme());
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.layout.mainprefs);
+
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setHomeButtonEnabled(true);
+		actionBar.setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(com.actionbarsherlock.view.MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			Intent intent = new Intent(this, MainWindow.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 }
