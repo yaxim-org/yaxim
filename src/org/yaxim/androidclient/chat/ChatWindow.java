79132b642ced1092921424f6895ea456c329761b
Dario Ernst <daddel9@nebuk.de>
2014-04-18 20:48:47 +0200

    fix: updating contact status on service connection to show correct buddy availability

--- src/org/yaxim/androidclient/chat/ChatWindow.java
+++ src/org/yaxim/androidclient/chat/ChatWindow.java
@@ -207,6 +207,7 @@ public class ChatWindow extends SherlockListActivity implements OnKeyListener,
 						mWithJabberID);
 				
 				mChatServiceAdapter.clearNotifications(mWithJabberID);
+				updateContactStatus();
 			}
 
 			public void onServiceDisconnected(ComponentName name) {
@@ -665,6 +666,7 @@ public class ChatWindow extends SherlockListActivity implements OnKeyListener,
 			mSubTitle.setVisibility((status_message != null && status_message.length() != 0)?
 					View.VISIBLE : View.GONE);
 			mSubTitle.setText(status_message);
+			
 			if (mChatServiceAdapter == null || !mChatServiceAdapter.isServiceAuthenticated())
 				status_mode = 0; // override icon if we are offline
 			mStatusMode.setImageResource(StatusMode.values()[status_mode].getDrawableId());
