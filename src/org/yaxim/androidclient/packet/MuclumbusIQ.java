6b5b0b30be1eac613fe3475e3027b7cf8330cf35
Georg Lukas <georg@op-co.de>
2019-02-21 10:53:05 +0100

    MUClumbus: implement search and result IQs

---
+++ src/org/yaxim/androidclient/packet/MuclumbusIQ.java
@@ -0,0 +1,91 @@
+package org.yaxim.androidclient.packet;
+
+import org.jivesoftware.smack.packet.IQ;
+import org.jivesoftware.smack.provider.IQProvider;
+import org.jivesoftware.smackx.xdata.FormField;
+import org.jivesoftware.smackx.xdata.packet.DataForm;
+import org.jivesoftware.smackx.xdata.provider.DataFormProvider;
+import org.xmlpull.v1.XmlPullParser;
+
+/**
+ * Implements the protocol currently used to search MUCs on a MUClumbus bot.
+ *
+ * This part is the request IQ and parser.
+ *
+ * Documentation at https://search.jabber.network/docs/api
+ *
+ * @author Georg Lukas
+ */
+
+public class MuclumbusIQ extends IQ {
+	public final static String ELEMENT = "search";
+	public final static String NAMESPACE = "https://xmlns.zombofant.net/muclumbus/search/1.0";
+
+	DataForm searchform;
+
+	public MuclumbusIQ(DataForm searchform) {
+		super(ELEMENT, NAMESPACE);
+		this.searchform = searchform;
+		if (searchform != null)
+			addExtension(searchform);
+	}
+	public MuclumbusIQ() {
+		this(null);
+	}
+
+
+	public static MuclumbusIQ searchFor(String query) {
+		DataForm sf = new DataForm(DataForm.Type.submit);
+		FormField fieldType = new FormField(FormField.FORM_TYPE);
+		fieldType.setType(FormField.Type.hidden);
+		fieldType.addValue(NAMESPACE + "#params");
+		sf.addField(fieldType);
+		FormField fieldKey = new FormField("key");
+		fieldKey.addValue("nusers");
+		sf.addField(fieldKey);
+		FormField queryKey = new FormField("q");
+		queryKey.addValue(query);
+		sf.addField(queryKey);
+		return new MuclumbusIQ(sf);
+	}
+
+	public DataForm getSearchForm() {
+		return searchform;
+	}
+
+	@Override
+	protected IQChildElementXmlStringBuilder getIQChildElementBuilder(IQChildElementXmlStringBuilder xml) {
+		xml.rightAngleBracket();
+		return xml;
+	}
+
+	public static class Provider extends IQProvider<MuclumbusIQ> {
+		@Override
+		public MuclumbusIQ parse(XmlPullParser parser, int initialDepth) throws Exception {
+			DataForm dataForm = null;
+
+			outerloop: while (true) {
+				final int eventType = parser.next();
+				final String name = parser.getName();
+
+				switch (eventType) {
+					case XmlPullParser.START_TAG:
+						switch (name) {
+							case DataForm.ELEMENT:
+								dataForm = DataFormProvider.INSTANCE.parse(parser);
+								break;
+						}
+						break;
+					case XmlPullParser.END_TAG:
+						if (parser.getDepth() == initialDepth) {
+							break outerloop;
+						}
+						break;
+				}
+			}
+
+			return new MuclumbusIQ(dataForm);
+		}
+
+	}
+}
---
+++ src/org/yaxim/androidclient/packet/MuclumbusResult.java
@@ -0,0 +1,129 @@
+/**
+ *
+ * Copyright 2003-2007 Jive Software.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.yaxim.androidclient.packet;
+
+import org.jivesoftware.smack.packet.IQ;
+import org.jivesoftware.smack.packet.SimpleIQ;
+import org.jivesoftware.smack.provider.IQProvider;
+
+import org.jxmpp.jid.EntityBareJid;
+import org.jxmpp.jid.impl.JidCreate;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implements the protocol currently used to search MUCs on a MUClumbus bot.
+ *
+ * This part is the response IQ and parser.
+ *
+ * Documentation at https://search.jabber.network/docs/api
+ *
+ * @author Georg Lukas
+ */
+public class MuclumbusResult extends SimpleIQ {
+
+    public static final String ELEMENT = "result";
+
+    /**
+     * List of search result items.
+     */
+    List<Item> items;
+
+    /**
+     * Creates a new IQ result with a list of items.
+     */
+    public MuclumbusResult(List<Item> items) {
+        super(ELEMENT, MuclumbusIQ.NAMESPACE);
+        this.items = items;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public static class Item {
+        public EntityBareJid address;
+        public String name;
+        public String description;
+        public int nusers;
+        public String language;
+        public boolean is_open;
+        public String anonymity_mode;
+    }
+
+    protected static Item parseItem(XmlPullParser parser, EntityBareJid jid) throws XmlPullParserException, IOException {
+        boolean done = false;
+        Item item = new Item();
+        item.address = jid;
+        while (!done) {
+            int eventType = parser.next();
+            if ((eventType == XmlPullParser.START_TAG) && ("name".equals(parser.getName()))) {
+                item.name = parser.nextText();
+            }
+            else if ((eventType == XmlPullParser.START_TAG) && ("description".equals(parser.getName()))) {
+                item.description = parser.nextText();
+            }
+            else if ((eventType == XmlPullParser.START_TAG) && ("nusers".equals(parser.getName()))) {
+                item.nusers = Integer.valueOf(parser.nextText()).intValue();
+            }
+            else if ((eventType == XmlPullParser.START_TAG) && ("language".equals(parser.getName()))) {
+                item.language = parser.nextText();
+            }
+            else if ((eventType == XmlPullParser.START_TAG) && ("is-open".equals(parser.getName()))) {
+                item.is_open = true;
+            }
+            else if ((eventType == XmlPullParser.START_TAG) && ("anonymity-mode".equals(parser.getName()))) {
+                item.anonymity_mode = parser.nextText();
+            }
+            else if (eventType == XmlPullParser.END_TAG && "item".equals(parser.getName())) {
+                done = true;
+            }
+        }
+        return item;
+	}
+
+    /**
+     * Result parsing Provider.
+     */
+    public static class Provider extends IQProvider<IQ> {
+
+        // FIXME this provider does return two different types of IQs
+        @Override
+        public IQ parse(XmlPullParser parser, int initialDepth) throws Exception {
+            ArrayList<Item> items = new ArrayList<>();
+
+            boolean done = false;
+
+            while (!done) {
+                int eventType = parser.next();
+                if (eventType == XmlPullParser.START_TAG && parser.getName().equals("item")) {
+                    EntityBareJid jid = JidCreate.entityBareFrom(parser.getAttributeValue("", "address"));
+                    items.add(parseItem(parser, jid));
+                }
+                else if (eventType == XmlPullParser.END_TAG && parser.getName().equals(ELEMENT)) {
+                    done = true;
+                }
+            }
+            return new MuclumbusResult(items);
+        }
+    }
+
+}
