e051e012e47a9147ac1c6ced5f863555db785dec
Georg Lukas <georg@op-co.de>
2019-04-05 17:31:42 +0200

    Prefs: immediately apply theme to prefs

--- src/org/yaxim/androidclient/preferences/MainPrefs.java
+++ src/org/yaxim/androidclient/preferences/MainPrefs.java
@@ -1,16 +1,19 @@
 package org.yaxim.androidclient.preferences;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import android.preference.PreferenceManager;
 import android.view.MenuItem;
 import android.support.v7.app.ActionBar;
 
 import org.yaxim.androidclient.MainWindow;
 import org.yaxim.androidclient.R;
 import org.yaxim.androidclient.YaximApplication;
+import org.yaxim.androidclient.util.PreferenceConstants;
 
-public class MainPrefs extends AppCompatPreferenceActivity {
+public class MainPrefs extends AppCompatPreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 	public void onCreate(Bundle savedInstanceState) {
 		setTheme(YaximApplication.getConfig().getTheme());
 		super.onCreate(savedInstanceState);
@@ -19,6 +22,22 @@ public class MainPrefs extends AppCompatPreferenceActivity {
 		ActionBar actionBar = getSupportActionBar();
 		actionBar.setHomeButtonEnabled(true);
 		actionBar.setDisplayHomeAsUpEnabled(true);
+		// track theme changes dynamically
+		PreferenceManager.getDefaultSharedPreferences(this)
+				.registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	protected void onDestroy() {
+		PreferenceManager.getDefaultSharedPreferences(this)
+				.unregisterOnSharedPreferenceChangeListener(this);
+		super.onDestroy();
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+		if (s.equals(PreferenceConstants.THEME))
+			recreate();
 	}
 
 	@Override
