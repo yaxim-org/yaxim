c608e7b11bbccdab2a13fd8c4ee1209bbd2009a7
Georg Lukas <georg@op-co.de>
2019-04-10 14:38:41 +0200

    android-remote-stacktrace-logger fork re-added

--- src/org/yaxim/androidclient/util/ErrorReportManager.java
+++ src/org/yaxim/androidclient/util/ErrorReportManager.java
@@ -1,9 +1,13 @@
 package org.yaxim.androidclient.util;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.util.Log;
 
 import org.yaxim.androidclient.YaximApplication;
+
+import ee.smmv.trace.ExceptionHandler;
 
 /**
  * This class is responsible for centrally managing error / crash reports and for sending them
@@ -31,13 +35,20 @@ public class ErrorReportManager {
 
 	public ErrorReportManager(Context ctx) {
 		this.ctx = ctx;
-		//this.handler = new DefaultExceptionHandler(NOP_HANDLER);
+		String appVersion = "unknown";
+		try {
+			PackageInfo packageInfo = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), 0);
+			appVersion = packageInfo.versionName;
+		} catch (PackageManager.NameNotFoundException e) {
+		}
+		final String filePath = ctx.getDir("stacktraces", 0).getAbsolutePath();
+		this.handler = new ExceptionHandler(NOP_HANDLER, appVersion, filePath, false);
 	}
 
 	public void report(Throwable e) {
 		if (!YaximApplication.getInstance().getConfig().reportCrash)
 			return;
-		//handler.uncaughtException(Thread.currentThread(), e);
+		handler.uncaughtException(Thread.currentThread(), e);
 		sendReports();
 	}
 	public void sendReports() {
