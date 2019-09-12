405d9368075f12182a88cd60fb05a3220e430132
Georg Lukas <georg@op-co.de>
2019-09-12 18:19:50 +0200

    Oob: implement optional desc element

--- src/org/yaxim/androidclient/packet/Oob.java
+++ src/org/yaxim/androidclient/packet/Oob.java
@@ -18,9 +18,15 @@ public class Oob implements ExtensionElement {
 	public final static String NAMESPACE = "jabber:x:oob";
 	public final static String ELEMENT = "x";
 	private String url;
+	private String desc;
 
 	public Oob(String url) {
 		this.url = url;
+	}
+
+	public Oob(String url, String desc) {
+		this.url = url;
+		this.desc = desc;
 	}
 
 	public String getElementName() {
@@ -35,18 +41,31 @@ public class Oob implements ExtensionElement {
 		return url;
 	}
 
+	public void setUrl(String url) {
+		this.url = StringUtils.requireNotNullOrEmpty(url, "URL must not be empty");
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
 	public CharSequence toXML(String enclosingNamespace) {
 		XmlStringBuilder xml = new XmlStringBuilder(this);
-		if (url != null) {
-			xml.rightAngleBracket().element("url", url).closeElement(ELEMENT);
-		} else
-			xml.closeEmptyElement();
+		xml.rightAngleBracket().element("url", url);
+		if (!StringUtils.isEmpty(desc))
+			xml.element("desc", desc);
+		xml.closeElement(ELEMENT);
 		return xml;
 	}
 
 	public static class Provider extends ExtensionElementProvider<Oob> {
 		public Oob parse(XmlPullParser parser, int initialDepth) throws Exception {
 			String url = null;
+			String desc = null;
 			boolean done = false;
 			while (!done) {
 				parser.next();
@@ -54,13 +73,14 @@ public class Oob implements ExtensionElement {
 				if (parser.getEventType() == XmlPullParser.START_TAG) {
 					if ("url".equals(elementName)) {
 						url = parser.nextText();
+					} else if ("desc".equals(elementName)) {
+						desc = parser.nextText();
 					}
-				}
-				else if (parser.getEventType() == XmlPullParser.END_TAG && ELEMENT.equals(elementName)) {
+				} else if (parser.getEventType() == XmlPullParser.END_TAG && ELEMENT.equals(elementName)) {
 					done = true;
 				}
 			}
-			return new Oob(url);
+			return new Oob(url, desc);
 		}
 	}
 }
